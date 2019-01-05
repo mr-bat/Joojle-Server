7e14cd1dc36c92b515ee0c1b2d78e328acc39bd4
Mahan TafreshiPour <mahantp19@gmail.com>
2019-01-05 20:04:17 +0330

    Add auth middleware to /event routes.

--- routes.js
+++ routes.js
@@ -5,5 +5,5 @@ exports = module.exports = (app) => {
     app.use('/user', routes.user);
     app.use('/poll_item', middlewares.auth, routes.pollItem);
     app.use('/poll', routes.poll);
-    app.use('/event', routes.event);
+    app.use('/event', middlewares.auth, routes.event);
 };
